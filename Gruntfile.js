9aafc1898eaa259c5ddc846e38dc876545843e37
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2014-12-29 18:21:17 +0100

    Removed download files, and simplified

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,40 +3,12 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  grunt.loadNpmTasks('grunt-downloadfile');
   grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-sass-convert');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    downloadfile: {
-      options:{
-        dest: './twentyfifteen-child/media/downloads'
-      },
-      files: [
-        global['url'] + 'wp-content/themes/twentyfifteen/style.css'
-      ]
-    },
-
-    'sass-convert': {
-      files: {
-        from: 'css',
-        to: 'scss',
-        cwd: 'twentyfifteen-child/media/downloads',
-        src: '*.css',
-        filePrefix: '_',
-        dest: 'twentyfifteen-child/media/sass/vendor'
-      }
-    },
 
     sass: {
       dist: {
@@ -44,7 +16,7 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: [
-          {'twentyfifteen-child/style.css': 'twentyfifteen-child/media/sass/style.scss'}
+          {'twentyfifteen-child/style.css': 'twentyfifteen-child/media/scss/style.scss'}
         ]
       }
     },
@@ -53,7 +25,7 @@ module.exports = function(grunt) {
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
-        files: 'twentyfifteen-child/media/sass/**/*.scss',
+        files: 'twentyfifteen-child/media/scss/**/*.scss',
         tasks: ['sass', 'concat']
       },
     },
@@ -68,14 +40,8 @@ module.exports = function(grunt) {
 
 
   grunt.registerTask('default', [
-    'downloadfile',
-    'sass-convert',
     'sass',
     'concat',
     'watch'
   ]);
-
-  grunt.registerTask('set_global', 'Set a global variable.', function() {
-    global['url'] = 'http://fabbrucci.me/';
-  });
 };
